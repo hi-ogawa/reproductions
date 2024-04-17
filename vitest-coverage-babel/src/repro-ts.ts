4c5146ca3e635f92b2e4ee3c5ca25b3bfb388461
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-04-17 09:05:47 +0900

    chore: add vitest-coverage-babel

---
+++ vitest-coverage-babel/src/repro-ts.ts
@@ -0,0 +1,4 @@
+import reproJson from "./repro-json.json" assert { type: "json" };
+// import reproJson from "./repro-json.json" with { type: "json" };
+
+export const reproTsReExport = reproJson;
---
+++ vitest-coverage-babel/src/repro.test.ts
@@ -0,0 +1,6 @@
+import { it } from "vitest";
+import { reproTsReExport } from "./repro-ts.js"
+
+it("repro", () => {
+  console.log(reproTsReExport);
+});
