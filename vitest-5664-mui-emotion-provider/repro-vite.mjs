cdbbf4f9b406965b8d33138d6b1eefefc15f04ba
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-08 16:27:35 +0900

    chore: more vite ssr

--- vitest-5664-mui-emotion-provider/repro-vite.mjs
+++ vitest-5664-mui-emotion-provider/repro-vite.mjs
@@ -3,7 +3,41 @@ import { createServer } from "vite";
 const server = await createServer({
 	clearScreen: false,
 	configFile: false,
+	server: {
+		preTransformRequests: false,
+	},
 });
+await server.pluginContainer.buildStart({});
 
-await server.ssrLoadModule("/repro-node.mjs");
+if (process.argv[2] === "reverse") {
+	console.log("######");
+	console.log("###### server.pluginContainer.resolveId");
+	console.log("######");
+	console.log(
+		await server.pluginContainer.resolveId("@emotion/react", undefined, {
+			ssr: true,
+		}),
+	);
+
+	console.log("######");
+	console.log("###### server.ssrLoadModule");
+	console.log("######");
+	await server.ssrLoadModule("/repro-node.mjs");
+
+} else {
+	console.log("######");
+	console.log("###### server.ssrLoadModule");
+	console.log("######");
+	await server.ssrLoadModule("/repro-node.mjs");
+
+	console.log("######");
+	console.log("###### server.pluginContainer.resolveId");
+	console.log("######");
+	console.log(
+		await server.pluginContainer.resolveId("@emotion/react", undefined, {
+			ssr: true,
+		}),
+	);
+}
+
 await server.close();
