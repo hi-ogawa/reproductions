37d6ec3aad8a0ae492e4a04367679ab97763a99f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-09 09:39:21 +0900

    chore: cleanup

--- shiki-604-cloudflare/src/app.tsx
+++ shiki-604-cloudflare/src/app.tsx
@@ -1,13 +1,21 @@
 import { defineComponent } from "vue";
-import { getHighlighterCore, type HighlighterCore } from "shiki/core";
+import { getHighlighterCore, loadWasm, type HighlighterCore } from "shiki/core";
 
 let highlighter: HighlighterCore;
 
 export const App = defineComponent(async () => {
+	await loadWasm(async (info) => {
+		if (import.meta.env.DEV) {
+			return import("shiki/wasm");
+		} else {
+			const mod = await import("shiki/onig.wasm" as string);
+			return WebAssembly.instantiate(mod.default, info);
+		}
+	});
+
 	highlighter ??= await getHighlighterCore({
 		themes: [import("shiki/themes/vitesse-light.mjs")],
 		langs: [import("shiki/langs/vue.mjs")],
-		loadWasm: import("shiki/wasm"),
 	});
 
 	const html = highlighter.codeToHtml(CODE, {
@@ -20,10 +28,12 @@ export const App = defineComponent(async () => {
 			style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
 		>
 			<h4>Shiki + Cloudflare Workers + Vite SSR</h4>
-			<div
-				style={{ border: "1px solid #aaa", padding: "0 1rem 1rem 1rem" }}
-				innerHTML={html}
-			/>
+			<div>
+				<div
+					style={{ border: "1px solid #aaa", padding: "0 1rem 1rem 1rem" }}
+					innerHTML={html}
+				/>
+			</div>
 		</div>
 	);
 });
--- shiki-604-cloudflare/vite.config.ts
+++ shiki-604-cloudflare/vite.config.ts
@@ -4,8 +4,6 @@ import {
 	vitePluginSsrMiddleware,
 } from "@hiogawa/vite-plugin-ssr-middleware";
 import { defineConfig } from "vite";
-import fs from "node:fs";
-import { createRequire } from "node:module";
 
 export default defineConfig((env) => ({
 	clearScreen: false,
@@ -15,32 +13,24 @@ export default defineConfig((env) => ({
 			entry: process.env["SERVER_ENTRY"] || "/src/adapters/node",
 			preview: path.resolve("./dist/server/index.js"),
 		}),
-		{
-			name: "replace-shiki-wasm",
-			apply: (_config, env) =>
-				Boolean(env.isSsrBuild && process.env["SERVER_ENTRY"]),
+		!!process.env["SERVER_ENTRY"] && {
+			name: "wrangler-wasm",
+			apply: (_config, env) => !!env.isSsrBuild,
 			config() {
 				return {
 					build: {
-						ssrEmitAssets: true,
+						rollupOptions: {
+							external: [/\.wasm$/],
+						},
 					},
 				};
 			},
-			async generateBundle(_options, bundle) {
-				for (const [_k, v] of Object.entries(bundle)) {
-					if (v.type === "chunk" && v.code.includes(`import("shiki/wasm")`)) {
-						const require = createRequire(import.meta.url);
-						const shikiWasmPath = require.resolve("shiki/onig.wasm");
-						this.emitFile({
-							fileName: "shiki.wasm",
-							type: "asset",
-							source: await fs.promises.readFile(shikiWasmPath),
-						});
-						// TODO: relative from v.fileName
-						v.code = v.code.replace(
-							`import("shiki/wasm")`,
-							`import("./shiki.wasm")`,
-						);
+			configResolved(config) {
+				// disable Vite's wasm integration since it doesn't allow wrangler's wasm bundling
+				// https://github.com/vitejs/vite/blob/f71ba5b94a6e862460a96c7bf5e16d8ae66f9fe7/packages/vite/src/node/plugins/wasm.ts#L78
+				for (const plugin of config.plugins) {
+					if (plugin.name === "vite:wasm-fallback") {
+						delete plugin.load;
 					}
 				}
 			},
