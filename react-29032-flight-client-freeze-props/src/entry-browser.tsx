5edae3e6fe39c20250b4958e3527b74d5def92fd
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-17 17:57:33 +0900

    chore: hydration

--- react-29032-flight-client-freeze-props/src/entry-browser.tsx
+++ react-29032-flight-client-freeze-props/src/entry-browser.tsx
@@ -1,3 +1,23 @@
-async function main() {}
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import type { StreamData } from "./entry-server";
+import { createMemoImport } from "./runtime-client";
+
+async function main() {
+	const flightStream = (globalThis as any).__flightStreamScript;
+
+	// react client (flight -> react node)
+	(globalThis as any).__webpack_require__ = createMemoImport();
+	const { default: ReactClient } = await import(
+		"react-server-dom-webpack/client.browser"
+	);
+	const node =
+		await ReactClient.createFromReadableStream<StreamData>(flightStream);
+
+	// react dom browser (react node -> html)
+	React.startTransition(() => {
+		ReactDOMClient.hydrateRoot(document, node);
+	});
+}
 
 main();
--- react-29032-flight-client-freeze-props/src/entry-ssr.tsx
+++ react-29032-flight-client-freeze-props/src/entry-ssr.tsx
@@ -9,24 +9,57 @@ export default async function handler(
 	_req: http.IncomingMessage,
 	res: http.ServerResponse,
 ) {
+	// react server (react node -> flight)
 	const reactServer = await importReactServer();
 	const flightStream = await reactServer.handler();
+	const [flightStream1, flightStream2] = flightStream.tee();
 
+	// react client (flight -> react node)
 	(globalThis as any).__webpack_require__ = createMemoImport();
 	const { default: ReactClient } = await import(
 		"react-server-dom-webpack/client.edge"
 	);
 	const node = await ReactClient.createFromReadableStream<StreamData>(
-		flightStream,
+		flightStream1,
 		{
 			ssrManifest: {},
 		},
 	);
+
+	// react dom ssr (react node -> html)
 	const ssrStream = await ReactDOMServer.renderToReadableStream(node, {
 		bootstrapModules: ["/@vite/client", "/src/entry-browser"],
 	});
-	res.setHeader("content-type", "text/html;charset=utf-8");
-	Readable.fromWeb(ssrStream as any).pipe(res);
+
+	// simplify streaming
+	let ssrString = await streamToString(ssrStream);
+	const flightString = await streamToString(flightStream2);
+	ssrString = ssrString.replace(
+		"<head>",
+		() => `<head>
+<script>
+	self.__flightStreamScript = new ReadableStream({
+		start(ctrl) {
+			ctrl.enqueue(${JSON.stringify(flightString)});
+			ctrl.close();
+		}
+	}).pipeThrough(new TextEncoderStream());
+</script>
+`,
+	);
+	res.setHeader("content-type", "text/html;charset=utf-8").end(ssrString);
+}
+
+async function streamToString(stream: ReadableStream<Uint8Array>) {
+	let result = "";
+	await stream.pipeThrough(new TextDecoderStream()).pipeTo(
+		new WritableStream({
+			write(chunk) {
+				result += chunk;
+			},
+		}),
+	);
+	return result;
 }
 
 async function importReactServer(): Promise<typeof import("./entry-server")> {
