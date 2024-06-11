c2acd78ad2aedd8f49129115175afbb780a50223
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-11 14:57:20 +0900

    wip: add vercel-one-click-deploy

---
+++ vercel-one-click-deploy/misc/vercel/build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -eu -o pipefail
+
+# .vercel/
+#   project.json
+#   output/
+#     config.json
+#     static/              = build/client
+#     functions/
+#       index.func/
+#         .vc-config.json
+#         index.mjs         = app/adapters/vercel-serverless.ts
+
+this_dir="$(dirname "${BASH_SOURCE[0]}")"
+
+# clean
+rm -rf .vercel/output
+mkdir -p .vercel/output
+
+# config.json
+cp "$this_dir"/config.json .vercel/output/config.json
+
+# static
+mkdir -p .vercel/output/static
+cp -r dist/client/. .vercel/output/static
+rm -rf .vercel/output/static/index.html
+
+# functions
+mkdir -p .vercel/output/functions/index.func
+cp "$this_dir"/.vc-config.json .vercel/output/functions/index.func/.vc-config.json
+
+# NOTE: silence `ignored-bare-import` for
+# https://rollupjs.org/configuration-options/#output-hoisttransitiveimports
+# https://rollupjs.org/faqs/#why-do-additional-imports-turn-up-in-my-entry-chunks-when-code-splitting
+# https://github.com/evanw/esbuild/issues/2334
+
+npx esbuild dist/server/index.js \
+  --outfile=.vercel/output/functions/index.func/index.mjs \
+  --metafile=dist/esbuild-metafile.json \
+  --log-override:ignored-bare-import=silent \
+  --define:process.env.NODE_ENV='"production"' \
+  --banner:js="import { createRequire } from 'module'; const require = createRequire(import.meta.url);" \
+  --bundle \
+  --minify \
+  --format=esm \
+  --platform=node
