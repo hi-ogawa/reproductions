799bf53538dfd4fb5b7aa43a16a2f6a764b46154
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-11 14:44:42 +0900

    wip: esbuild

--- vite-ssr-vercel-json/misc/vc-build.js
+++ vite-ssr-vercel-json/misc/vc-build.js
@@ -1,5 +1,12 @@
-function main() {
-	import.meta.dirname;
+import { cp, rm } from "node:fs/promises";
+import * as esbuild from "esbuild";
+
+async function main() {
+  await rm("dist/client/index.html");
+  await rm("api", { recursive: true, force: true })
+  await cp("dist/server", "api", { recursive: true });
+  // TODO
+  esbuild;
 }
 
 main();
