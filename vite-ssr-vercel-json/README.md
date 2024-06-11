5f9fbb3a9c4513d1c496afd20a8cce4ab6434cd5
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-11 12:55:26 +0900

    chore: add vite-ssr-vercel-json

---
+++ vite-ssr-vercel-json/README.md
@@ -0,0 +1,9 @@
+```sh
+pnpm dev
+```
+
+## links
+
+- https://vercel.com/docs/projects/project-configuration
+- https://vercel.com/docs/functions/functions-api-reference
+- https://vercel.com/docs/functions/edge-middleware/middleware-api
---
+++ vite-ssr-vercel-json/vite.config.ts
@@ -0,0 +1,21 @@
+import path from "node:path";
+import {
+	vitePluginLogger,
+	vitePluginSsrMiddleware,
+} from "@hiogawa/vite-plugin-ssr-middleware";
+import { defineConfig } from "vite";
+
+export default defineConfig((env) => ({
+	clearScreen: false,
+	plugins: [
+		vitePluginLogger(),
+		vitePluginSsrMiddleware({
+			entry: "/src/entry-server.tsx",
+			preview: path.resolve("./dist/server/index.js"),
+			mode: "ViteRuntime-no-hmr",
+		}),
+	],
+	build: {
+		outDir: env.isSsrBuild ? "dist/server" : "dist/client",
+	},
+}));
