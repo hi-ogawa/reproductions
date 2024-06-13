98bcd088b726bbac2faf7b539340a97a817c1a9b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-13 15:19:08 +0900

    chore: update vite-hono-hmr

--- vite-hono-hmr/src/App.tsx
+++ vite-hono-hmr/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = React.useState(0)
 
   return (
     <>
@@ -17,6 +17,7 @@ export default function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>{React.version}</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
