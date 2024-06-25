2bd8106aa522e91dc640d73c215e3a17efc2f80c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-06-25 16:28:58 +0900

    chore: test next alias package

--- vite-next/vite.config.ts
+++ vite-next/vite.config.ts
@@ -1,8 +1,6 @@
-import { vitePluginReactServerNext } from "@hiogawa/react-server/next/plugin";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { vitePluginReactServerNext } from "next/plugin";
 
-export default defineConfig({
+export default {
   clearScreen: false,
-  plugins: [react(), vitePluginReactServerNext()],
-});
+  plugins: [vitePluginReactServerNext()],
+};
