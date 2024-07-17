1e20c327fedfbca1707480437be46e48e12fd8f8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-18 08:11:24 +0900

    chore: debug vitest-plugin-react-v2

--- vitest-plugin-react-v2/repro-vite.js
+++ vitest-plugin-react-v2/repro-vite.js
@@ -1,3 +1,4 @@
+import path from "node:path";
 import { createServer } from "vite";
 
 async function main() {
@@ -10,7 +11,18 @@ async function main() {
 			},
 		},
 	});
+
+	// [not ok]
+	// vite:resolve 0.92ms /src/repro.js -> /home/hiroshi/code/personal/reproductions/vitest-plugin-react-v2/src/repro.js +0ms
+	// vite:resolve 0.23ms ./generated.js -> null +6ms
+	// vite:resolve 0.19ms /src/generated.js -> null +5ms
 	await server.ssrLoadModule("/src/repro.js");
+
+	// [ok]
+	// vite:resolve 0.24ms ./generated.js -> null +0ms
+	// vite:resolve 0.21ms /home/hiroshi/code/personal/reproductions/vitest-plugin-react-v2/src/generated.js -> null +6ms
+	// await server.ssrLoadModule(path.resolve("src/repro.js"));
+
 	await server.close();
 }
 
