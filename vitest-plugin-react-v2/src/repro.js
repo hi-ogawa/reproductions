c1a29a8f8cda18dae4874fbd4f0d0efc81b7fac1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-30 19:17:09 +0900

    chore: tweak vitest-plugin-react-v2

--- vitest-plugin-react-v2/src/repro.js
+++ vitest-plugin-react-v2/src/repro.js
@@ -1,9 +1,5 @@
 import { writeFile } from "node:fs/promises";
 
-async function main() {
-	await writeFile("src/generated.js", "export default 'hello'");
-	const mod = await import("./generated.js");
-	console.log(mod);
-}
-
-main();
+await writeFile("src/generated.js", "export default 'hello'");
+const mod = await import("./generated.js");
+console.log(mod);
