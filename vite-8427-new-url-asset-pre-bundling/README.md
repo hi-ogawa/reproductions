f5c40a56e837328aeffa53bf227b9a09ab47a6f7
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-04 17:02:45 +0900

    feat: add new-url-import-meta-url-replace

--- vite-8427-new-url-asset-pre-bundling/README.md
+++ vite-8427-new-url-asset-pre-bundling/README.md
@@ -3,5 +3,5 @@
 - https://github.com/users/hi-ogawa/projects/4/views/1?pane=issue&itemId=73410910
 
 ```sh
-pnpm dev
+pnpm dev --force
 ```
--- vite-8427-new-url-asset-pre-bundling/vite.config.ts
+++ vite-8427-new-url-asset-pre-bundling/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import fs from "node:fs";
+import MagicString from "magic-string";
+import path from "node:path";
 
 export default defineConfig({
 	clearScreen: false,
@@ -7,20 +9,37 @@ export default defineConfig({
 		esbuildOptions: {
 			plugins: [
 				{
-					name: "debug",
+					name: "new-url-import-meta-url-replace",
 					setup(build) {
 						build.onLoad({ filter: /\.js$/ }, async (args) => {
 							const data = await fs.promises.readFile(args.path, "utf-8");
-							if (
-								data.includes("new URL") &&
-								data.includes("import.meta.url")
-							) {
-								args.path;
-								data;
-								return {
-									loader: "js",
-									contents: data,
-								};
+							if (data.includes("import.meta.url")) {
+								// replace
+								//   new URL("./vite.svg", import.meta.url)
+								// with
+								//   new URL("/(absolute-path-to)/vite.svg", import.meta.url)
+								// https://github.com/vitejs/vite/blob/0f56e1724162df76fffd5508148db118767ebe32/packages/vite/src/node/plugins/assetImportMetaUrl.ts#L51-L52
+								const assetImportMetaUrlRE =
+									/\bnew\s+URL\s*\(\s*('[^']+'|"[^"]+"|`[^`]+`)\s*,\s*import\.meta\.url\s*(?:,\s*)?\)/dg;
+								const matches = data.matchAll(assetImportMetaUrlRE);
+								const output = new MagicString(data);
+								for (const match of matches) {
+									const rawUrl = match[1]!;
+									const url = rawUrl.slice(1, -1);
+									if (url[0] === ".") {
+										const absUrl = path.resolve(args.path, "..", url);
+										if (fs.existsSync(absUrl)) {
+											const [start, end] = match.indices![1]!;
+											output.update(start, end, JSON.stringify(absUrl));
+										}
+									}
+								}
+								if (output.hasChanged()) {
+									return {
+										loader: "js",
+										contents: output.toString(),
+									};
+								}
 							}
 							return null;
 						});
