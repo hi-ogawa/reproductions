3908b71c7a5ca2ff9a0e43d8b072ea14ec0f2f14
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-05 11:50:37 +0900

    chore: add test-dep-cjs

--- vite-ssr-cjs-astro-napi-rs-2206/astro.config.ts
+++ vite-ssr-cjs-astro-napi-rs-2206/astro.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+// @ts-ignore
+import testDepCjs from "test-dep-cjs";
+console.log(testDepCjs);
+
 // https://astro.build/config
 export default defineConfig({});
