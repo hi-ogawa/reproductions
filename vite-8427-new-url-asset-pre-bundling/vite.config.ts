b25b68fc38819104c6e19808b528842e7110d756
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-05 13:35:35 +0900

    chore(vite-8427-new-url-asset-pre-bundling): use "file" namespace

--- vite-8427-new-url-asset-pre-bundling/vite.config.ts
+++ vite-8427-new-url-asset-pre-bundling/vite.config.ts
@@ -16,34 +16,37 @@ export default defineConfig({
 					//   new URL("/(absolute-path-to)/asset.svg", import.meta.url)
 					name: "new-url-import-meta-url-replace",
 					setup(build) {
-						build.onLoad({ filter: /\.js$/ }, async (args) => {
-							const data = await fs.promises.readFile(args.path, "utf-8");
-							if (data.includes("import.meta.url")) {
-								// https://github.com/vitejs/vite/blob/0f56e1724162df76fffd5508148db118767ebe32/packages/vite/src/node/plugins/assetImportMetaUrl.ts#L51-L52
-								const assetImportMetaUrlRE =
-									/\bnew\s+URL\s*\(\s*('[^']+'|"[^"]+"|`[^`]+`)\s*,\s*import\.meta\.url\s*(?:,\s*)?\)/dg;
-								const matches = data.matchAll(assetImportMetaUrlRE);
-								const output = new MagicString(data);
-								for (const match of matches) {
-									const rawUrl = match[1]!;
-									const url = rawUrl.slice(1, -1);
-									if (url[0] !== "/") {
-										const absUrl = path.resolve(path.dirname(args.path), url);
-										if (fs.existsSync(absUrl)) {
-											const [start, end] = match.indices![1]!;
-											output.update(start, end, JSON.stringify(absUrl));
+						build.onLoad(
+							{ filter: /\.js$/, namespace: "file" },
+							async (args) => {
+								const data = await fs.promises.readFile(args.path, "utf-8");
+								if (data.includes("import.meta.url")) {
+									// https://github.com/vitejs/vite/blob/0f56e1724162df76fffd5508148db118767ebe32/packages/vite/src/node/plugins/assetImportMetaUrl.ts#L51-L52
+									const assetImportMetaUrlRE =
+										/\bnew\s+URL\s*\(\s*('[^']+'|"[^"]+"|`[^`]+`)\s*,\s*import\.meta\.url\s*(?:,\s*)?\)/dg;
+									const matches = data.matchAll(assetImportMetaUrlRE);
+									const output = new MagicString(data);
+									for (const match of matches) {
+										const rawUrl = match[1]!;
+										const url = rawUrl.slice(1, -1);
+										if (url[0] !== "/") {
+											const absUrl = path.resolve(path.dirname(args.path), url);
+											if (fs.existsSync(absUrl)) {
+												const [start, end] = match.indices![1]!;
+												output.update(start, end, JSON.stringify(absUrl));
+											}
 										}
 									}
+									if (output.hasChanged()) {
+										return {
+											loader: "js",
+											contents: output.toString(),
+										};
+									}
 								}
-								if (output.hasChanged()) {
-									return {
-										loader: "js",
-										contents: output.toString(),
-									};
-								}
-							}
-							return null;
-						});
+								return null;
+							},
+						);
 					},
 				},
 			],
