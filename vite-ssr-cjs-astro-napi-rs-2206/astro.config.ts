f27b6666fc9a5494a1241218115b0514c36464e8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-05 13:54:39 +0900

    chore(vite-ssr-cjs-astro-napi-rs-2206): add workaround

--- vite-ssr-cjs-astro-napi-rs-2206/astro.config.ts
+++ vite-ssr-cjs-astro-napi-rs-2206/astro.config.ts
@@ -4,5 +4,10 @@ import { defineConfig } from 'astro/config';
 import testDepCjs from "test-dep-cjs";
 console.log(testDepCjs);
 
+// workaround is to use `createRequire`
+// import { createRequire } from "node:module";
+// const require = createRequire(import.meta.url);
+// console.log(require("test-dep-cjs"))
+
 // https://astro.build/config
 export default defineConfig({});
