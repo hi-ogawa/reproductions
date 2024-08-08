608779da4d00e92701e4df13cd22fd7ac5a25992
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-08 14:41:49 +0900

    chore: test worker prebundle

--- vite-sqlite-wasm/vite.config.js
+++ vite-sqlite-wasm/vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { vitePluginPreBundleNewUrl } from "@hiogawa/vite-plugin-pre-bundle-new-url";
 
 export default defineConfig({
   server: {
     headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
     },
   },
-  optimizeDeps: {
-    exclude: ['@sqlite.org/sqlite-wasm'],
-  },
+  // optimizeDeps: {
+  //   exclude: ['@sqlite.org/sqlite-wasm'],
+  // },
+  plugins: [
+    vitePluginPreBundleNewUrl({
+      debug: true,
+    }),
+  ],
 });
