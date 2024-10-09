03fccc2dda2e05cf3e7b2c7bb8a02998a1d5fd5e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-10-09 12:13:00 +0900

    chore: tweak

--- vite-perf-resolve-cache/src/bench.js
+++ vite-perf-resolve-cache/src/bench.js
@@ -4,17 +4,15 @@ import { createServer } from "vite";
 async function main() {
   const server = await createServer({
     configFile: false,
-    plugins: [
-      {
-        name: "repro",
-        resolveId() {},
-      },
-    ],
     optimizeDeps: {
       noDiscovery: true,
       include: [],
     },
+    server: {
+      middlewareMode: true,
+    },
   });
+  await server.pluginContainer.buildStart({});
 
   const { dependencies } = JSON.parse(readFileSync("./package.json", "utf-8"));
   for (const id in dependencies) {
