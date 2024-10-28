79111d78dd2a62575894293534e67c81dd0076bc
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-10-28 12:45:59 +0900

    chore: test deno-vite-plugin

--- vite-ssr-deno-jsr/repro.mjs
+++ vite-ssr-deno-jsr/repro.mjs
@@ -1,9 +1,24 @@
 import { createServer } from "vite";
+import deno from "@deno/vite-plugin";
 
 const server = await createServer({
   configFile: false,
+  optimizeDeps: {
+    noDiscovery: true,
+  },
+  plugins: [
+    deno(),
+    // {
+    //   name: 'repro',
+    //   resolveId(source, importer, options) {
+    //     if (source === '@std/assert') {
+    //       return { id: source, external: true  }
+    //     }
+    //   }
+    // }
+  ],
   ssr: {
-    external: ["jsr:@std/assert@^1.0.0"],
+    // external: ['@std/assert']
   },
 });
 await server.listen();
--- vite-ssr-deno-jsr/src/entry.js
+++ vite-ssr-deno-jsr/src/entry.js
@@ -1,7 +1,12 @@
-// import { assertEquals } from "jsr:@std/assert@^1.0.0";
 // import chalk from "npm:chalk@5";
+// import { assertEquals } from "jsr:@std/assert@^1.0.0";
+// import chalk from "chalk";
 import { assertEquals } from "@std/assert";
-import chalk from "chalk";
 
-console.log(chalk.yellow("Hello world"));
-assertEquals(1, 2);
+// console.log(chalk.yellow("Hello world"));
+
+try {
+  assertEquals(1, 2);
+} catch (e) {
+  console.error(e);
+}
