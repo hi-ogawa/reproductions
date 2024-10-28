87ff72c966aaf94dc626ec1a2145e00363665511
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-10-28 13:50:49 +0900

    wip

--- vite-ssr-deno-jsr/repro.mjs
+++ vite-ssr-deno-jsr/repro.mjs
@@ -1,4 +1,4 @@
-import { createServer } from "vite";
+import { createServer, createServerModuleRunner, DevEnvironment } from "vite";
 import deno from "@deno/vite-plugin";
 
 const server = await createServer({
@@ -17,10 +17,37 @@ const server = await createServer({
     //   }
     // }
   ],
-  ssr: {
-    // external: ['@std/assert']
+  // ssr: {
+  //   external: ['@std/assert']
+  // },
+  environments: {
+    custom: {
+      dev: {
+        createEnvironment: (name, config, context) => {
+          class MyEnvironment extends DevEnvironment {
+            async fetchModule(id, importer, options) {
+              console.log({ id, importer });
+              // TODO: deno info to resolve external?
+              // https://github.com/denoland/deno-vite-plugin/blob/895724634320eb5946c8c606f2e6f342976b4c0c/src/resolver.ts#L80
+              // if (id === "@std/assert") {
+              //   return { externalize: id, type: "builtin" };
+              // }
+              return super.fetchModule(id, importer, options);
+            }
+          }
+          return new MyEnvironment(name, config, context);
+        },
+      },
+    },
   },
 });
 await server.listen();
-await server.ssrLoadModule("/src/entry");
+// await server.ssrLoadModule("/src/entry");
+
+const runner = createServerModuleRunner(server.environments.custom, {
+  sourcemapInterceptor: "prepareStackTrace",
+  hmr: false,
+});
+await runner.import("/src/entry");
+
 await server.close();
