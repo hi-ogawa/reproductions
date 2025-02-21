64100571ba3556eafb47b6a0a977398315f1a84b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-02-21 10:55:07 +0900

    wip: output esbuild

--- oxc-sourcemap-debug/src/app.tsx
+++ oxc-sourcemap-debug/src/app.tsx
@@ -1,73 +1,75 @@
 import React from "react";
-import type { TransformResult } from "rolldown/experimental";
+import { ApiResult } from "../vite.config";
 
 export function App() {
   const [input, setInput] = React.useState("");
-  const [result, setResult] = React.useState<TransformResult>();
+  const [result, setResult] = React.useState<ApiResult>();
 
   const handleSubmit = () =>
     React.startTransition(async () => {
-      const res = await fetch("/api/oxc?input=" + encodeURIComponent(input));
+      const res = await fetch(
+        "/api/transform?" + new URLSearchParams({ input }),
+      );
       if (res.ok) {
-        const result = await res.json();
-        setResult(result);
+        setResult(await res.json());
       } else {
-        alert("'/api/oxc' failed\n" + (await res.text()));
+        alert("Transform failed\n" + (await res.text()));
       }
     });
 
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}>
       <h4>Oxc transform source map debugger</h4>
-      <form
-        action={async () => {
-          const res = await fetch(
-            "/api/oxc?input=" + encodeURIComponent(input),
-          );
-          if (res.ok) {
-            const result = await res.json();
-            console.log(result);
+      <label>Input</label>
+      <textarea
+        rows={10}
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && (e.ctrlKey || e.metaKey)) {
+            e.preventDefault();
+            handleSubmit();
           }
         }}
-      >
-        <label>
-          Input <br />
-          <textarea
-            rows={20}
-            cols={80}
-            value={input}
-            onChange={(e) => {
-              setInput(e.target.value);
-            }}
-            onKeyDown={(e) => {
-              if (e.key === "Enter" && e.ctrlKey) {
-                e.preventDefault();
-                handleSubmit();
-              }
-            }}
-          />
-        </label>
-      </form>
+      />
       <label>
-        Output{" "}
-        {result?.map && (
+        Oxc output{" "}
+        {result?.oxc.map && (
           <a
-            href={generateSourcemapVisualizerLink(result.code, result.map)}
+            href={generateSourcemapVisualizerLink(
+              result.oxc.code,
+              JSON.stringify(result.oxc.map),
+            )}
             target="_blank"
           >
             (source map)
           </a>
         )}
-        <br />
-        <textarea rows={20} cols={80} readOnly value={result?.code} />
       </label>
+      <textarea rows={10} readOnly value={result?.oxc.code} />
+      <label>
+        Esbuild output{" "}
+        {result?.esbuild.map && (
+          <a
+            href={generateSourcemapVisualizerLink(
+              result.esbuild.code,
+              result.esbuild.map,
+            )}
+            target="_blank"
+          >
+            (source map)
+          </a>
+        )}
+      </label>
+      <textarea rows={10} readOnly value={result?.esbuild.code} />
     </div>
   );
 }
 
-function generateSourcemapVisualizerLink(code: string, map: unknown) {
-  const mapJson = JSON.stringify(map);
-  const hashRaw = `${code.length}\0${code}${mapJson.length}\0${mapJson}`;
+function generateSourcemapVisualizerLink(code: string, map: string) {
+  const hashRaw = `${code.length}\0${code}${map.length}\0${map}`;
   const hash = btoa(hashRaw);
   return `https://evanw.github.io/source-map-visualization/#` + hash;
 }
--- oxc-sourcemap-debug/vite.config.ts
+++ oxc-sourcemap-debug/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { transform } from "rolldown/experimental";
+import * as esbuild from "esbuild";
 
 export default defineConfig({
   plugins: [
@@ -10,15 +11,15 @@ export default defineConfig({
       configureServer(server) {
         server.middlewares.use(async (req, res, next) => {
           const url = new URL(req.url ?? "/", "http://localhost");
-          if (url.pathname === "/api/oxc") {
-            const input = url.searchParams.get("input")!;
-            const result = transform("test.js", input, {
-              sourceType: "module",
-              lang: "tsx",
-              sourcemap: true,
-            });
-            res.setHeader("Content-Type", "application/json");
-            res.end(JSON.stringify(result));
+          if (url.pathname === "/api/transform") {
+            try {
+              const result = await apiHandler(url);
+              res.setHeader("Content-Type", "application/json");
+              res.end(JSON.stringify(result));
+            } catch (e) {
+              res.statusCode = 500;
+              res.end(e instanceof Error ? e.message : "Unknown error");
+            }
             return;
           }
           next();
@@ -27,3 +28,20 @@ export default defineConfig({
     },
   ],
 });
+
+async function apiHandler(url: URL) {
+  const input = url.searchParams.get("input")!;
+  const resultOxc = transform("test.js", input, {
+    sourceType: "module",
+    lang: "tsx",
+    sourcemap: true,
+  });
+  const resultEsbuild = await esbuild.transform(input, {
+    format: "esm",
+    loader: "tsx",
+    sourcemap: true,
+  });
+  return { oxc: resultOxc, esbuild: resultEsbuild };
+}
+
+export type ApiResult = Awaited<ReturnType<typeof apiHandler>>;
