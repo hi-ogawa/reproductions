0df1f303207717f60c0ebf8ca74f2b3eb370d5d8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-03-15 18:37:12 +0900

    chore: test tar package

--- oxc-wasi-vite-2/vite.config.ts
+++ oxc-wasi-vite-2/vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   optimizeDeps: {
-    exclude: ["oxc-parser", "@oxc-parser/binding-wasm32-wasi"],
+    esbuildOptions: {
+      // napi-rs wasi-browser.js output relies on top-level await
+      target: "esnext",
+    },
+    // force optimize if linked dep
+    // include: ["oxc-parser"],
   },
   server: {
     headers: {
@@ -10,34 +15,34 @@ export default defineConfig({
       "Cross-Origin-Opener-Policy": "same-origin",
     },
   },
-  resolve: {
-    // requires when testing linked package of oxc/napi/parser
-    alias: {
-      "oxc-parser": "oxc-parser/wasm.js",
-    },
-    dedupe: ["@oxc-parser/binding-wasm32-wasi"],
-  },
-  plugins: [
-    {
-      name: "wasi-workaround",
-      enforce: "pre",
-      transform(code, id) {
-        // TODO: Vite bug? sometimes, the request with `?import&url` fails as Vite responds wasm binary.
-        //   node_modules/@oxc-parser/binding-wasm32-wasi/parser.wasm32-wasi.wasm?import&url
-        // Using `new URL(...)` seems to work more consistently.
-        if (
-          id.includes("@oxc-parser/binding-wasm32-wasi/parser.wasi-browser.js")
-        ) {
-          return code.replace(
-            `import __wasmUrl from './parser.wasm32-wasi.wasm?url'`,
-            `const __wasmUrl = new URL("./parser.wasm32-wasi.wasm", import.meta.url);`,
-          );
-        }
-      },
-    },
-  ],
+  // requires when testing linked package of oxc/napi/parser
+  // resolve: {
+  //   alias: {
+  //     "oxc-parser": "oxc-parser/wasm.js",
+  //   },
+  //   dedupe: ["@oxc-parser/binding-wasm32-wasi"],
+  // },
+  // plugins: [
+  //   {
+  //     name: "wasi-workaround",
+  //     enforce: "pre",
+  //     transform(code, id) {
+  //       // TODO: Vite bug? sometimes, the request with `?import&url` fails as Vite responds wasm binary.
+  //       //   node_modules/@oxc-parser/binding-wasm32-wasi/parser.wasm32-wasi.wasm?import&url
+  //       // Using `new URL(...)` seems to work more consistently.
+  //       if (
+  //         id.includes("@oxc-parser/binding-wasm32-wasi/parser.wasi-browser.js")
+  //       ) {
+  //         return code.replace(
+  //           `import __wasmUrl from './parser.wasm32-wasi.wasm?url'`,
+  //           `const __wasmUrl = new URL("./parser.wasm32-wasi.wasm", import.meta.url);`,
+  //         );
+  //       }
+  //     },
+  //   },
+  // ],
   build: {
     // napi-rs wasi-browser.js output relies on top-level await
-    target: "es2022",
+    target: "esnext",
   },
 });
