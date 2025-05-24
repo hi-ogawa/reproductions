2c36f960b194710166dfd3fd6532bcf29a9b19ba
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-05-24 19:10:40 +0900

    chore: test next client reference code split

--- next-rsc-client-css/app/page.jsx
+++ next-rsc-client-css/app/page.jsx
@@ -1,7 +1,14 @@
 import Link from "next/link";
-import { TestClient } from "./client";
+
+// js split  [no]
+// css split [no]
+// import { TestClient } from "./client";
 
 export default async function Page({ searchParams }) {
+  // js split  [no]
+  // css split [no]
+  const { TestClient } = await import("./client")
+
   return (
     <div style={{ border: "1px solid blue", padding: "20px" }}>
       <h4>Page</h4>[<Link href="/">show</Link> |{" "}
