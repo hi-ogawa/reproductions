e321f03ee5ab34b63c1ff19a09bd570fab70ce5b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-05-25 18:20:53 +0900

    chore: cleanup

--- next-rsc-client-css/app/client.jsx
+++ next-rsc-client-css/app/client.jsx
@@ -1,12 +1,7 @@
 "use client";
 
 import "./client.css";
-import large from "./large";
 
 export function TestClient() {
-  if (Math.random() > 1) {
-    console.log(large);
-  }
-
   return <div className="test-client">[test-client-style]</div>;
 }
