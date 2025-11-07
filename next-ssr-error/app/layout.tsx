de02a5736a4de8bafb5f432b29c6af534c341be7
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-11-07 16:58:33 +0900

    chore: update next-ssr-error (notFound runtime)

--- next-ssr-error/app/layout.tsx
+++ next-ssr-error/app/layout.tsx
@@ -12,6 +12,12 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           <li>
             <Link href="/test-error">/test-error</Link>
           </li>
+          <li>
+            <Link href="/test-not-found">/test-not-found</Link>
+          </li>
+          <li>
+            <Link href="/test-not-found-runtime">/test-not-found-runtime</Link>
+          </li>
         </ul>
         {children}
       </body>
