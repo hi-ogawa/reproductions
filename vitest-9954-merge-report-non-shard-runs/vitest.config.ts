2cf5aec73af0f308899b103a79c37383edd962d1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-03-24 17:19:17 +0900

    chore: cleanup

--- vitest-9954-merge-report-non-shard-runs/vitest.config.ts
+++ vitest-9954-merge-report-non-shard-runs/vitest.config.ts
@@ -3,26 +3,13 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     name: process.env.TEST_NAME,
+    projects: process.env.MERGE_TEST_NAMES
+      ? JSON.parse(process.env.MERGE_TEST_NAMES).map((name: string) => ({
+          extends: true,
+          test: {
+            name,
+          },
+        }))
+      : undefined,
   },
-  plugins: [
-    {
-      name: "non-shard-merger",
-      enforce: "pre",
-      config() {
-        if (process.env.MERGE_TEST_NAMES) {
-          const names: string[] = JSON.parse(process.env.MERGE_TEST_NAMES);
-          return {
-            test: {
-              projects: names.map((name) => ({
-                extends: true,
-                test: {
-                  name,
-                },
-              })),
-            },
-          };
-        }
-      },
-    },
-  ],
 });
